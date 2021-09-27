a1345ca50b3be39a904beb8861b5f57f93968cd8
psm651 <psm651@naver.com>
2021-09-27 21:23:58 +0900

    Initial commit

---
+++ src/main/java/pineapple/pinemvc/PineMvcApplication.java
@@ -0,0 +1,16 @@
+package pineapple.pinemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class PineMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PineMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pineapple/pinemvc/PineMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pineapple.pinemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PineMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
