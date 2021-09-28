15593d7967882a4f47b6aab4ac92c536ac444862
psm651 <psm651@naver.com>
2021-09-28 23:40:33 +0900

    db연결, index page thymeleaf연결

---
+++ src/main/java/pineapple/pinemvc/controller/Home.java
@@ -0,0 +1,12 @@
+package pineapple.pinemvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
