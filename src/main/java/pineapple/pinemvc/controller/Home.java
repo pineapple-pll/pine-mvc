73909dc732f0fb30dd5844f1847ee5012735634c
psm651 <psm651@naver.com>
2021-10-06 23:46:30 +0900

    회원가입, 로그아웃 처리

--- src/main/java/pineapple/pinemvc/controller/Home.java
+++ src/main/java/pineapple/pinemvc/controller/Home.java
@@ -1,12 +1,27 @@
 package pineapple.pinemvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Home {
-    @GetMapping("/")
+    //@GetMapping("/")
     public String home() {
         return "index";
     }
+
+    @GetMapping("/")
+    public String homeLogin(
+            @CookieValue(value = "memberToken", required = false) String memberToken,
+            Model model) {
+        if (memberToken == null) {
+            return "page-login";
+        }
+        //TODO: 회원인지 확인
+
+        return "index";
+    }
+
 }
--- src/main/java/pineapple/pinemvc/controller/MemberController.java
+++ src/main/java/pineapple/pinemvc/controller/MemberController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import pineapple.pinemvc.dto.Member;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,7 +35,7 @@ public class MemberController {
     }
 
     @PostMapping("/do_login")
-    public String do_login(Member member, Model model) throws JsonProcessingException {
+    public String doLogin(Member member, Model model, HttpServletResponse response) throws JsonProcessingException {
         // 헤더설정
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
@@ -50,12 +53,30 @@ public class MemberController {
         // RestTemplate의 exchange 메서드를 통해 URL에 HttpEntity와 함께 요청
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.exchange("http://218.145.110.135:8000/auth/members/signin", HttpMethod.POST, entity, String.class);
+        //ResponseEntity<String> responseEntity = restTemplate.exchange("http://127.0.0.1:8001/auth/members/signin", HttpMethod.POST, entity, String.class);
+
+        Cookie idCookie = new Cookie("memberToken", String.valueOf(responseEntity.getBody()));
+        idCookie.setPath("/");
+        idCookie.setMaxAge(60 * 30);//60초 30분
+        response.addCookie(idCookie);
 
         // 요청후 응답 확인
         System.out.println(responseEntity.getStatusCode());
         System.out.println(responseEntity.getBody());
 
-        return "index";
+        return "redirect:/";
     }
 
+    @PostMapping("/do_logout")
+    public String doLogout(HttpServletResponse response) {
+        expireCookie(response, "memberToken");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
 }
