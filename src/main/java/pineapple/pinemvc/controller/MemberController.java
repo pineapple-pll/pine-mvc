6c2a716f24094a4a7184f04fa3e69d3af660d28c
psm651 <psm651@naver.com>
2021-10-07 23:36:21 +0900

    환경별 property분리

--- src/main/java/pineapple/pinemvc/controller/MemberController.java
+++ src/main/java/pineapple/pinemvc/controller/MemberController.java
@@ -3,6 +3,8 @@ package pineapple.pinemvc.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +14,24 @@ import pineapple.pinemvc.dto.Member;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
 import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/member")
+@PropertySource("classpath:application-${spring.profiles.active}.properties")
 public class MemberController {
+
+    @Value("${pineapple.authServer}")
+    private String authServer;
 
     @Autowired
     private ObjectMapper objectMapper;
+
 
     @GetMapping("/login")
     public String login() {
@@ -52,7 +61,16 @@ public class MemberController {
 
         // RestTemplate의 exchange 메서드를 통해 URL에 HttpEntity와 함께 요청
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange("http://218.145.110.135:8000/auth/members/signin", HttpMethod.POST, entity, String.class);
+        String loginUrl =  "/auth/members/signin";
+        String loginUrl2 = authServer + loginUrl;
+
+        System.out.println(loginUrl2);
+        System.out.println(loginUrl2);
+        System.out.println(loginUrl);
+        System.out.println(loginUrl);
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(loginUrl2, HttpMethod.POST, entity, String.class);
+        //ResponseEntity<String> responseEntity = restTemplate.exchange("http://218.145.110.135:8000/auth/members/signin", HttpMethod.POST, entity, String.class);
         //ResponseEntity<String> responseEntity = restTemplate.exchange("http://127.0.0.1:8001/auth/members/signin", HttpMethod.POST, entity, String.class);
 
         Cookie idCookie = new Cookie("memberToken", String.valueOf(responseEntity.getBody()));
